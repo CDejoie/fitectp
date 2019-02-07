c1a75ea4ce4febc412f118010bb4346bff078efd
cldudouyt@live.fr <cldudouyt@live.fr>
2019-02-07 09:26:42 +0100

    API Student Done

---
+++ src/ContosoUniversity/Controllers/StudentsController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using System.Web.Mvc;
+using ContosoUniversity.DAL;
+using ContosoUniversity.DTO;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Controllers
+{
+    public class StudentsController : ApiController
+    {
+        private SchoolContext db = new SchoolContext();
+
+      
+        
+
+        // GET: api/Students/5
+        //[ResponseType(typeof(Student))]
+     
+        public IHttpActionResult GetStudent(int id)
+        {
+
+            try
+            {
+                //Création d'un étudiant et de la mise en relation avec l'ID
+                Student student = db.Students.FirstOrDefault(x => x.ID == id);
+                // Création d'un student DTO ou semble il un view model d'après mes documentations
+                StudentsDTO studentDTO = new StudentsDTO();
+                List<Dictionary<string, string>> courseList = new List<Dictionary<string, string>>();
+
+                // parcours Enrollment
+                foreach (Enrollment enrollment in student.Enrollments)
+                {
+                    // Création du dictionnaire course
+                    Dictionary<string, string> course = new Dictionary<string, string>();
+                    // Parcours l'enrollment par le course ID
+                    course["CourseID"] = enrollment.CourseID.ToString();
+                    // ajoute une un cours  au dictionnaire lorsqur CoursID Correspond
+                    courseList.Add(course);
+                }
+                //Affichage des données
+                studentDTO.ID = student.ID;
+                studentDTO.FirstMidName = student.FirstMidName;
+                studentDTO.LastName = student.LastName;
+                studentDTO.EnrollmentDate = student.EnrollmentDate;
+                studentDTO.Enrollments = courseList;
+
+                return Ok(studentDTO);
+            }
+            catch
+            {
+                return NotFound();
+
+            }
+        }
+
+       
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool StudentExists(int id)
+        {
+            return db.People.Count(e => e.ID == id) > 0;
+        }
+    }
+}
---
+++ src/ContosoUniversity/DTO/StudentsDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.DTO
+{
+    public class StudentsDTO
+    {
+
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public List<Dictionary<string, string>> Enrollments { get; set; }
+
+
+    }
+}
